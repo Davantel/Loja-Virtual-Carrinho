db76465b781a937790185a1a415ce6db76591c69
caiod <caiodavantel@hotmail.com>
2020-03-28 19:16:38 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'LVC';
+  title = 'LojaVirtualCarrinho';
+
+  produtos = [
+    {nome: "Produto 1",
+    preco: 100,
+    quantidade: 1},
+    {nome: "Produto 2",
+    preco: 150,
+    quantidade: 1},
+  ];
+
+  carrinho = [];
+
+  soma=0;
+
+  comprar(produto){
+    if (this.carrinho.indexOf(produto) === -1){
+      this.carrinho.push(produto);
+    }
+
+    else{
+      produto.quantidade+=1
+    }
+    this.soma+=produto.preco;
+  }
 }
